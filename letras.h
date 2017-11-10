1ef9254663c57ab19d8099fb87e6210bf7f17cf6
AlexisArroyo <31545675+AlexisArroyo@users.noreply.github.com>
2017-11-10 01:57:45 -0600

    Add files via upload

---
+++ letras.h
@@ -0,0 +1,48 @@
+void letraA(){
+	printf("AAAA\n");
+	printf("A  A\n");
+	printf("AAAA\n");
+	printf("A  A\n");
+	printf("A  A\n");
+	printf("    \n");
+}
+void letraL(){
+	printf("L   \n");
+	printf("L   \n");
+	printf("L   \n");
+	printf("L   \n");
+	printf("LLLL\n");
+	printf("    \n");
+}
+void letraE(){
+	printf("EEEE\n");
+	printf("E   \n");
+	printf("EEEE\n");
+	printf("E   \n");
+	printf("EEEE\n");
+	printf("    \n");
+}
+void letraX(){
+	printf("X  X\n");
+	printf(" XX \n");
+	printf(" XX \n");
+	printf(" XX \n");
+	printf("X  x\n");
+	printf("    \n");
+}
+void letraI(){
+	printf("IIII\n");
+	printf(" II \n");
+	printf(" II \n");
+	printf(" II \n");
+	printf("IIII\n");
+	printf("    \n");
+}
+void letraS(){
+	printf("SSSS\n");	
+	printf("S   \n");
+	printf("SSSS\n");
+	printf("   S\n");
+	printf("SSSS\n");
+	printf("    \n");
+}
---
+++ tarea21.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "cabecera.h"
+#include "letras.h"
+
+
+	int main(){
+	  appInfoData("Tarea 21","14/11/2017");	  
+
+	  letraA();
+	  letraL();
+	  letraE();
+	  letraX();
+	  letraI();
+	  letraS();
+}
